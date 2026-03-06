b44fc83f087ec61520006075768a349b4b17b566
eduardobj1905 <eduardobernal1502@gmail.com>
2026-03-06 10:48:02 +0100

    Realizacion de conteo de arrays 2.0

--- Desktop/ejerciciosPHP/conteo.php
+++ Desktop/ejerciciosPHP/conteo.php
@@ -1,26 +1,20 @@
 <?php
 
-// Almacenamos todos los arrays en la variable listas
-$arrays = [
-    [5, 12, 8, 20, 1], [1, 1, 2, 3, 5, 8, 13], [10, 10, 10, 10],
-    [19, 18, 17, 16, 15, 14, 13], [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
-    [1, 3, 5, 7, 9, 11, 13, 15, 17, 19], [7, 14, 7, 14, 7], [20, 1, 20, 1],
-    [11, 12, 13, 14, 15], [5, 10, 15, 20], [2, 3, 5, 7, 11, 13, 17, 19],
-    [9, 3, 1, 18, 4, 12, 7, 2, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    [15, 15, 16, 16, 17, 17], [4, 9, 16], [8, 16, 8, 4, 2, 1],
-    [20, 20, 20, 5, 5, 1], [6, 12, 18], [1, 2, 2, 3, 3, 3, 4, 4, 4, 4],
-    [13, 7, 19, 2, 11, 5, 17]
-];
 
-// Nos encargamos de unificar todos los arrays en uno grande
-$union = array_merge(...$arrays);
+
+function analizarEntradaUsuario(array $numeros) {
+
+if (empty($numeros)) {
+        echo "Error: El array no contiene datos.\n";
+        return;
+    }
 
 // Contamos el numero de elementos que hay en el array unificado y la suma total de estos
-$totalNumeros = count($union);
-$sumaTotal = array_sum($union);
+$totalNumeros = count($numeros);
+$sumaTotal = array_sum($numeros);
 
 //Empleamos array_count_values para ver la cantidad de veces que se repite un valor
-$conteoGlobal = array_count_values($union);
+$conteoGlobal = array_count_values($numeros);
 
 // Calculamos la moda ordenando de mayor a menor 
 arsort($conteoGlobal); 
@@ -45,8 +39,14 @@ echo "--- Conteo de repeticiones de cada numero ---\n";
 // Ordenamos de menor a mayor para facilitar la lectura
 ksort($conteoGlobal); 
 
-foreach ($conteoGlobal as $num => $veces) {
+ foreach ($conteoGlobal as $num => $veces) {
     // Si $veces es 1, no se repite se imprimira por pantalla aparece 1 vez
     $mensaje = ($veces > 1) ? "se repite $veces veces" : "aparece 1 vez";
     echo "El número $num $mensaje.\n";
+ }
 }
+
+$numeros =[20, 20, 20, 5, 5, 1];
+
+
+analizarEntradaUsuario( $numeros);
